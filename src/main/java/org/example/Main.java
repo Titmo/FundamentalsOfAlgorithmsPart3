541f5ce5db93757b6eb6a2616d2246036588bf9d
03den <03denchik03@gmail.com>
2023-08-07 21:15:57 +0300

    Остались тесты

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       StringListImpl stringList=new StringListImpl(2);
+        System.out.println("stringList.add(1) = " + stringList.add(1));
+        System.out.println("stringList.toArray() = " + Arrays.toString(stringList.toArray()));
+        System.out.println("stringList.add(2) = " + stringList.add(2));
+        System.out.println("stringList.toArray() = " + Arrays.toString(stringList.toArray()));
+        System.out.println("stringList.add(3) = " + stringList.add(3));
+        System.out.println("stringList.toArray() = " + Arrays.toString(stringList.toArray()));
+        System.out.println("stringList.size() = " + stringList.size());
     }
 }
